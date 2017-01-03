1c8f63df68a1fe6a179da735b9656fd436042d49
Theo Nolte <theno@gmx.net>
2017-01-03 18:32:31 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# vueapp
+
+> A Vue.js project
+
+Replay of the video tutorial [Vue.js 2.0 In 60 Minutes][1].
+
+[1]: https://www.youtube.com/watch?v=z6hQqgvGI4Y
+
+## Commands used in the tutorial
+
+```bash
+# Intall vue client
+npm install --global vue-cli
+
+# Create repo
+vue init webpack vueapp
+cd vueapp
+
+# Install dependencies
+npm install
+
+# serve at localhost
+npm run dev
+
+# Install more deps
+npm install vue-resource --save
+npm install vue-router --save
+```
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+```
+
+For detailed explanation on how things work, checkout the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ src/components/Test.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="test">
+    <input type="text" v-model="title"><br />
+    <h1>{{title}}</h1>
+    <p v-if="showName">{{user.firstName}}</p>
+    <p v-else>Nobody</p>
+    <ul>
+      <li v-for="item in items">{{item.title}}</li>
+    </ul>
+    <button v-on:click="greet('Hello World')">Say Greeting</button>
+    <br />
+    <input type="text" v-on:keyup="pressKey" v-on:keyup.enter="enterHit"></input>
+    <hr />
+    <label>First name: </label><input type="text" v-model="user.firstName"></input>
+    <br />
+    <label>Last name: </label><input type="text" v-model="user.lastName"></input>
+    <h3>{{fullName}}</h3>
+    <h2>{{msg}}</h2>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'test',
+    props: {
+      msg: {
+        type: String,
+        default: 'foobar'
+      }
+    },
+    data() {
+      return {
+        title: 'Hello, World',
+        user: {
+          firstName: 'John',
+          lastName: 'Doe',
+        },
+        showName: true,
+        items: [
+          {title: 'Item One'},
+          {title: 'Item Two'},
+          {title: 'Item Three'}
+        ]
+      }
+    },
+    methods: {
+      greet: function(greeting) {
+        alert(greeting);
+      },
+      pressKey: function(e) {
+        console.log(e.target.value);
+      },
+      enterHit: function() {
+        console.log('You hit enter');
+      }
+    },
+    computed: {
+      fullName: function() {
+        return this.user.firstName + ' ' + this.user.lastName;
+      }
+    }
+  }
+</script>
+
+<style scoped>
+</style>
